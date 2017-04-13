c9825bee58c45c9702f0a811d3c14c02c038d465
Daniel Hui <dah124@pitt.edu>
2017-04-12 22:33:06 -0400

    Add files via upload

--- assignment4.js
+++ assignment4.js
@@ -1,17 +1,60 @@
-// The anonymous function below will fire on page load
-
-// Some things to consider
-// $.ajax(); to make your requests a little easier. Or the vanilla js way, it's up to you.
-// $.on(); for event handling
-// Remember, selecting elements in jQuery is like selecting them in CSS
-// You'll probably have to manipulate some strings
-// some jQuery functions to help display results
-// $.show(), $.hide(), $.slideup(), $.slidedown(), $.fadein(), $.fadeout()
-// Add content from requests with something like
-// $.html(), $.text(), etc.
-// keyup events could be helpful to get value of field as the user types
-
 (function() {
-  // Magic!
-  console.log('Keepin\'n it clean with an external script!');
+
+	var in_glob = $(".flexsearch-input").after("<ul class='flexsearch-results'></ul>");
+	// results/messages container
+	var result_glob = $(".flexsearch-results");
+	// no data yet
+	result_glob.html("<p class='flexsearch-message'>No data loaded</p>");
+	var data = [];
+
+	//parse
+	$.when(
+		$.getJSON( 'http://www.mattbowytz.com/simple_api.json?data=programming', function(response){
+			$.each( response, function(key, val) {
+				console.log(val);
+				if(val !== 10 && val !== 200 && val.length > 2){
+					data =  data.concat(val);
+				}
+			});
+		}),
+
+		$.getJSON( 'http://www.mattbowytz.com/simple_api.json?data=interests', function(response){
+			$.each( response, function(key, val) {
+				if(val !== 9 && val !== 200 && val.length > 2){
+					data =  data.concat(val);
+				}
+			});
+		})
+
+
+	).then(function() {
+		//clear and add all results
+		result_glob.html("");
+		for(var i = 0; i < data.length; i++) {
+			result_glob.append("<li class='flexsearch-result'>" + data[i] + "</li>");
+		}
+
+		// bind input to autocomplete
+		in_glob.on("keyup", function(event){
+			// case insensitive
+			var input = in_glob.val().toLowerCase();
+			var results = [];
+
+			for(var i  = 0; i < data.length; i++) {
+				// if substr exists in word, add it to results
+				if(data[i].toLowerCase.includes(input)) {
+					results.push(data[i]);
+				}
+			}
+
+			//clear results container
+			result_glob.html("");
+			//display results
+			if(results.length) {
+				for(i = 0; i < results.length; i++) {
+					result_glob.append("<li class='flexsearch-result'>" + results[i] + "</a></li>");
+				}
+			}
+		});
+	});
 })();
